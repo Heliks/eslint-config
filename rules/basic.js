d9a86df7e165d89456a184c9f49f503389856e35
heliks <teh4.ews@gmail.com>
2020-03-15 12:35:32 +0100

    use indent 2 and add unicorn rules

--- rules/basic.js
+++ rules/basic.js
@@ -109,7 +109,7 @@ module.exports = {
   'wrap-iife': 'error',
   'yoda': 'error',
   'strict': 'error',
-  'init-declarations': 'error',
+  'init-declarations': 'off',
   'no-delete-var': 'error',
   'no-label-var': 'error',
   'no-restricted-globals': 'error',
@@ -192,10 +192,7 @@ module.exports = {
     'error',
     'beside'
   ],
-  'indent': [
-    'error',
-    4
-  ],
+  'indent': ['error', 2],
   'key-spacing': [
     'error',
     {
@@ -366,17 +363,57 @@ module.exports = {
     'never'
   ],
   'space-infix-ops': 'error',
-  'space-unary-ops': [
-    'error',
-    {
-      'overrides': {
-        '!': true
-      }
-    }
-  ],
+  'space-unary-ops': 'off',
   'spaced-comment': [
     'error',
     'always'
   ],
   'switch-colon-spacing': 'error',
+  "unicorn/better-regex": "error",
+  "unicorn/catch-error-name": "error",
+  "unicorn/consistent-function-scoping": "error",
+  "unicorn/custom-error-definition": "off",
+  "unicorn/error-message": "error",
+  "unicorn/escape-case": "error",
+  "unicorn/expiring-todo-comments": "error",
+  "unicorn/explicit-length-check": "error",
+  "unicorn/filename-case": "error",
+  "unicorn/import-index": "error",
+  "unicorn/new-for-builtins": "error",
+  "unicorn/no-abusive-eslint-disable": "error",
+  "unicorn/no-array-instanceof": "error",
+  "unicorn/no-console-spaces": "error",
+  "unicorn/no-fn-reference-in-iterator": "off",
+  "unicorn/no-for-loop": "error",
+  "unicorn/no-hex-escape": "error",
+  "unicorn/no-keyword-prefix": "error",
+  "unicorn/no-nested-ternary": "error",
+  "unicorn/no-new-buffer": "error",
+  "unicorn/no-process-exit": "error",
+  "unicorn/no-unreadable-array-destructuring": "error",
+  "unicorn/no-unsafe-regex": "error",
+  "unicorn/no-unused-properties": "error",
+  "unicorn/no-zero-fractions": "error",
+  "unicorn/number-literal-case": "error",
+  "unicorn/prefer-add-event-listener": "error",
+  "unicorn/prefer-dataset": "error",
+  "unicorn/prefer-event-key": "error",
+  "unicorn/prefer-flat-map": "error",
+  "unicorn/prefer-includes": "error",
+  "unicorn/prefer-modern-dom-apis": "error",
+  "unicorn/prefer-negative-index": "error",
+  "unicorn/prefer-node-append": "error",
+  "unicorn/prefer-node-remove": "error",
+  "unicorn/prefer-query-selector": "error",
+  "unicorn/prefer-reflect-apply": "error",
+  "unicorn/prefer-replace-all": "off",
+  "unicorn/prefer-spread": "error",
+  "unicorn/prefer-starts-ends-with": "error",
+  "unicorn/prefer-string-slice": "error",
+  "unicorn/prefer-text-content": "error",
+  "unicorn/prefer-trim-start-end": "error",
+  "unicorn/prefer-type-error": "error",
+  "unicorn/prevent-abbreviations": "error",
+  "unicorn/string-content": "off",
+  "unicorn/throw-new-error": "error"
 };
